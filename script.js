1f449dd705d237ffb62d782485318f1f0fff6a3a
portfolio-hayashi2428 <162471735+portfolio-hayashi2428@users.noreply.github.com>
2024-03-18 23:20:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ setTimeout(function () {
     document.getElementsByTagName("html")[0].classList.add("loading-delay");
   }
 }, 3000);
+
+//スクロールをしたら1度だけアニメーションをする設定
+$(
+  ".works , .introduction, .skill ,.about ,.contact ,.strengths ,.idea_source"
+).on("inview", function (event, isInView) {
+  if (isInView) {
+    //表示領域に入った時
+    $(this).addClass("active"); //クラス名が付与
+  }
+});
