79ff6f6ca911f641de7f4ec17b1c5344ac767ba4
portfolio-hayashi2428 <bfwah24@gmail.com>
2024-03-19 16:44:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ setTimeout(function () {
   }
 }, 3000);
 
-//スクロールをしたら1度だけアニメーションをする設定
+/*-------------------------------------------
+ふわっと表示 
+-------------------------------------------*/
 $(
   ".works , .introduction, .skill ,.about ,.contact ,.strengths ,.idea_source"
 ).on("inview", function (event, isInView) {
   if (isInView) {
-    //表示領域に入った時
-    $(this).addClass("active"); //クラス名が付与
+    $(this).addClass("active");
   }
 });
